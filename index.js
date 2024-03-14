da70646b36329de2ee08a2534c3c5c8d3317aba2
Noorul Misbah <noorulmisbaah.noor@gmail.com>
2024-03-14 11:28:57 +0100

    Timestamp v1.0.1

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/api/:date', (req, res) => {
   if (date.toString() === 'Invalid Date') {
     res.json({ error: 'Invalid Date' });
   } else {
-    res.json({ unix: Date.now(), utc: date.toUTCString()});
+    res.json({ unix: date.getTime(), utc: date.toUTCString()});
   }
 });
 
